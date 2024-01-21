183f324db906ade80fc0e1bbdcfc576732f2a5a4
harsh3452 <harshpatil3452@gmail.com>
2024-01-21 12:28:28 +0530

    basic calculator

---
+++ calculator.py
@@ -0,0 +1,11 @@
+num1 = int(input("Enter 1st Integer:"))
+num2 = int(input("Enter 2nd Integer:"))
+choice = str(input("Enter the operation to be performed (+,-,*,/):"))
+if(choice == '+'):
+   print(num1+num2)
+if(choice == '='):
+   print(num1-num2)
+if(choice == '*'):
+   print(num1*num2)
+if(choice == '/'):
+   print(num1/num2)
